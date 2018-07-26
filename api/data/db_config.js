f4ccee6d8d6ec183399e9837b62a4e2c7cf543c7
Darrel Idiagbor <idiagbordarrel@gmail.com>
2018-07-26 12:07:38 -0500

    chore: keep api

--- api/data/db_config.js
+++
@@ -1,6 +0,0 @@
-var crypto = require('crypto').randomBytes(256).toString('hex');
-
-module.exports = {
-    url: "mongodb://localhost:27017/rockies",
-    secret: 'Hello' //crypto
-}
--- api/data/models/locations.model.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var settingsSchema = new mongoose.Schema({
-    country: String,
-    states: [String],
-});
-
-mongoose.model('Location', settingsSchema, 'locations');
